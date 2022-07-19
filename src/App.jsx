3ed60126780e4a681457e42c5e0b135f13b0c788
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-19 03:08:51 -0500

    Feat: My information and button are added

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,14 @@ function App() {
         <img className='image' src='./image.jpg' alt='perfil'></img>
       </div>
       <div className='name'>
-        <h1>{i("name")}</h1>
+        <h1>{t("name")}</h1>
+      </div>
+      <div className='information'>
+        <p>{t("information")}</p>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {t("button")} {count}
         </button>
       </div>
     </div>
