af90de7dd694e33185f11ba70f4495ecb02c636e
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-19 02:55:21 -0500

    Feat: My name translator is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar.jsx'
+import { useTranslation } from 'react-i18next';
 function App() {
   const [count, setCount] = useState(0)
+  const { i } = useTranslation();
   return (
     <div className="App">
       <Navbar/>
       <div className='foto'>
         <img className='image' src='./image.jpg' alt='perfil'></img>
+      </div>
+      <div className='name'>
+        <h1>{i("name")}</h1>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
