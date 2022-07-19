edac60ca089e0850716fd3a7d1e4e312c3908d5f
MartinDeLaCruz.69 <martindelacruz169.com>
2022-07-19 02:55:30 -0500

    Feat: My name translator is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/navbar.jsx'
 import { useTranslation } from 'react-i18next';
 function App() {
   const [count, setCount] = useState(0)
-  const { i } = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="App">
       <Navbar/>
